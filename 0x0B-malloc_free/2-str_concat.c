b9c358d267bf5590183fbd0fd3a19b685446108b
HASHEM-S <popleeswager077@gmail.com>
2023-10-31 04:09:28 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 
 {
@@ -20,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 
 	if (s2 == NULL)
-		
+
 		s2 = "";
-		
+
 		i = ci = 0;
 
 	while (s1[i] != '\0')
